8ec3784f5d417c3b510c748dceee27e945a4c700
kiennt120 <kyriejad23@gmail.com>
2022-11-03 08:55:50 +0700

    fix

--- configs/yolov6l.py
+++ configs/yolov6l.py
@@ -8,14 +8,14 @@ model = dict(
         type='CSPBepBackbone',
         num_repeats=[1, 6, 12, 18, 6],
         out_channels=[64, 128, 256, 512, 1024],
-        csp_e=float(1)/2,
-        ),
+        csp_e=float(1) / 2,
+    ),
     neck=dict(
         type='CSPRepPANNeck',
         num_repeats=[12, 12, 12, 12],
         out_channels=[256, 128, 128, 256, 256, 512],
-        csp_e=float(1)/2,
-        ),
+        csp_e=float(1) / 2,
+    ),
     head=dict(
         type='EffiDeHead',
         in_channels=[128, 256, 512],
@@ -26,7 +26,7 @@ model = dict(
         strides=[8, 16, 32],
         iou_type='giou',
         use_dfl=True,
-        reg_max=16, #if use_dfl is False, please set reg_max to 0
+        reg_max=16,  # if use_dfl is False, please set reg_max to 0
         distill_weight={
             'class': 2.0,
             'dfl': 1.0,
@@ -34,7 +34,7 @@ model = dict(
     )
 )
 
-solver=dict(
+solver = dict(
     optim='SGD',
     lr_scheduler='Cosine',
     lr0=0.01,
@@ -51,13 +51,14 @@ data_aug = dict(
     hsv_s=0.7,
     hsv_v=0.4,
     degrees=0.0,
-    translate=0.1,
-    scale=0.9,
+    translate=0.0,
+    scale=1.0,
     shear=0.0,
     flipud=0.0,
-    fliplr=0.5,
-    mosaic=1.0,
+    fliplr=0.0,
+    mosaic=0.0,
     mixup=0.1,
+    blur=0.2,
 )
 training_mode = "conv_silu"
 # use normal conv to speed up training and further improve accuracy.
